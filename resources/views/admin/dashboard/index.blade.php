e6205c9010f905be6c98fe26d31dd13afcf55b90
thegreytangent <thegreytangent@gmail.com>
2023-10-14 07:32:04 +0800

    Feat: Remove social widgets

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -8,10 +8,6 @@
                 <div class="col-sm-12">
                     <div class="page-sub-header">
                         <h3 class="page-title">Welcome Admin!</h3>
-                        <ul class="breadcrumb">
-                            <li class="breadcrumb-item"><a href="index-2.html">Home</a></li>
-                            <li class="breadcrumb-item active">Admin</li>
-                        </ul>
                     </div>
                 </div>
             </div>
@@ -237,10 +233,8 @@
                         </div>
                     </div>
                 </div>
-
             </div>
             <div class="col-xl-6 d-flex">
-
                 <div class="card flex-fill comman-shadow">
                     <div class="card-header d-flex align-items-center">
                         <h5 class="card-title ">Student Activity </h5>
@@ -305,52 +299,6 @@
             </div>
         </div>
 
-        <div class="row">
-            <div class="col-xl-3 col-sm-6 col-12">
-                <div class="card flex-fill fb sm-box">
-                    <div class="social-likes">
-                        <p>Like us on facebook</p>
-                        <h6>50,095</h6>
-                    </div>
-                    <div class="social-boxs">
-                        <img src="{{asset("assets/img/icons/social-icon-01.svg")}}" alt="Social Icon">
-                    </div>
-                </div>
-            </div>
-            <div class="col-xl-3 col-sm-6 col-12">
-                <div class="card flex-fill twitter sm-box">
-                    <div class="social-likes">
-                        <p>Follow us on twitter</p>
-                        <h6>48,596</h6>
-                    </div>
-                    <div class="social-boxs">
-                        <img src="{{asset("assets/img/icons/social-icon-02.svg")}}" alt="Social Icon">
-                    </div>
-                </div>
-            </div>
-            <div class="col-xl-3 col-sm-6 col-12">
-                <div class="card flex-fill insta sm-box">
-                    <div class="social-likes">
-                        <p>Follow us on instagram</p>
-                        <h6>52,085</h6>
-                    </div>
-                    <div class="social-boxs">
-                        <img src="{{asset("assets/img/icons/social-icon-03.svg")}}" alt="Social Icon">
-                    </div>
-                </div>
-            </div>
-            <div class="col-xl-3 col-sm-6 col-12">
-                <div class="card flex-fill linkedin sm-box">
-                    <div class="social-likes">
-                        <p>Follow us on linkedin</p>
-                        <h6>69,050</h6>
-                    </div>
-                    <div class="social-boxs">
-                        <img src="{{asset("assets/img/icons/social-icon-04.svg")}}" alt="Social Icon">
-                    </div>
-                </div>
-            </div>
-        </div>
 
     </div>
 @endsection
--- resources/views/template/main.blade.php
+++ resources/views/template/main.blade.php
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<!-- Mirrored from preschool.dreamguystech.com/template/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 02 Jul 2023 08:45:25 GMT -->
 <head>
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
-    <title></title>
+    <title>Hybrid Learning</title>
 
     <link rel="shortcut icon" href="{{asset("assets/img/favicon.png")}}">
 
