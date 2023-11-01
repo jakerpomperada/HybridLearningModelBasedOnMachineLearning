2a3444c8394c3cb5795ca65278a8029a6536d322
thegreytangent <thegreytangent@gmail.com>
2023-11-01 10:13:09 +0800

    Feat: setup student quiz

--- app/Http/Controllers/Teacher/StudentQuizController.php
+++ app/Http/Controllers/Teacher/StudentQuizController.php
@@ -1,12 +1,67 @@
 <?php
-
-namespace App\Http\Controllers\Teacher;
-
-use App\Http\Controllers\Controller;
-
-class StudentQuizController extends Controller
-{
-    public function index() {
-		return 1;
-    }
-}
+	
+	namespace App\Http\Controllers\Teacher;
+	
+	use App\Http\Controllers\Controller;
+	use App\Repositories\TeacherRepository;
+	use App\Services\TeacherService;
+	use Domain\Modules\Student\Repositories\IStudentRepository;
+	
+	class StudentQuizController extends Controller
+	{
+		
+		
+		
+		use TeacherControllerTrait;
+		
+		protected TeacherService $teacherService;
+		protected TeacherRepository $teacherRepository;
+		protected IStudentRepository $studentRepository;
+		
+		
+		public function __construct(TeacherService $teacherService, TeacherRepository $teacherRepository, IStudentRepository $studentRepository)
+		{
+			$this->teacherService    = $teacherService;
+			$this->teacherRepository = $teacherRepository;
+			$this->studentRepository = $studentRepository;
+		}
+		public function index()
+		{
+			
+			$subject_load_id = request()->input('teaching_load_id');
+			
+			$subject_loads = $this->teacherService->getSubjectLoads($this->getTeacherId());
+			
+			if ($subject_load_id) {
+				$student_task_performance = $this->teacherRepository->GetAllStudentTaskPerformanceByTeachingLoadGroupByDate(
+					$subject_load_id
+				);
+				$student_task_performance = collect($student_task_performance->items())->map(function ($i) use ($subject_load_id) {
+					
+					return (object)[
+						'date'             => $i->displayDate(),
+						'year_section'     => $i->TeachingLoad->getYearSection(),
+						'subject'          => $i->TeachingLoad->Subject->code,
+						'title'            => $i->title,
+						'points'           => $i->points,
+						'teaching_load_id' => $i->teaching_load_id
+					];
+				});
+				
+				
+			} else {
+				$student_task_performance = [];
+			}
+			
+			
+			return view('teacher.student-quiz.index')->with([
+				'semester'               => $this->getCurrentTerm()->displaySemester(),
+				'term'                   => $this->getCurrentTerm()->getTerm(),
+				'subject_loads'          => $subject_loads,
+				'subject_load_id'        => $subject_load_id,
+				'student_participations' => $student_task_performance
+			]);
+			
+			
+		}
+	}
